a2e16f623d29a8a98db5f2b625499affb3058b0b
Wieser <jerwin.wieser@mediq.com>
2022-12-18 14:25:08 +0100

    update settings secret key and debug

--- indstats/settings.py
+++ indstats/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Deploy on heroku
 import django_heroku
 import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ww+a*e^xk1t+6h2g4_7xkfq7cd2^%lh7xyf2pjla#*99k58xs%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG') == 'True')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1:8000', 'indstats.herokuapp.com']
 
 
 # Application definition
