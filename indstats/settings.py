f4c1bda6fd21ebc57e69cc65f9e37712727de8c0
jerwinwieser <j2.wieser@gmail.com>
2022-12-20 23:37:45 +0100

    heroku server staticfiles fix middleware

--- indstats/settings.py
+++ indstats/settings.py
@@ -67,7 +67,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
+
 
 ROOT_URLCONF = 'indstats.urls'
 
