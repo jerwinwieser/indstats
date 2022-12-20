9712be3250381c038e0ab0151eaaef66c5365016
jerwinwieser <j2.wieser@gmail.com>
2022-12-20 23:30:23 +0100

    url error fix

--- indstats/urls.py
+++ indstats/urls.py
@@ -22,4 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('accounts/', include('allauth.urls')),
-] + + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
